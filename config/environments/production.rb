54e61517bff2df44c416966ea4400d5dfe5826af
Durrell Chamorro <durrell.chamorro@gmail.com>
2017-04-03 14:00:04 -0700

    Configure mailgun to send emails on Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,15 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'durrellsnetflix.herokuapp.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'durrellsnetflix.herokuapp.com' }
 end
