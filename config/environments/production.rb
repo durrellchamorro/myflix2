e3869330b74e416957055d9d0640ad8f3653fcdf
Durrell Chamorro <durrell.chamorro@gmail.com>
2017-06-22 15:31:46 -0700

    Configure to use ssl in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,19 +4,14 @@ Myflix::Application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   config.serve_static_files = false
-
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
-
   config.assets.compile = false
-
   config.assets.digest = true
-
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
+  config.force_ssl = true
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
